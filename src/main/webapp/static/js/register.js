d9fef3308365a60280e96dea62c9b9b37e93780d
SaarChaffee <1240148332@qq.com>
2021-12-25 17:04:04 +0800

    finish register js function

--- src/main/webapp/static/js/register.js
+++ src/main/webapp/static/js/register.js
@@ -8,7 +8,7 @@ var ramCode = () => {
 
 var ramColor = () => {
   return `rgb(${Math.round(Math.random() * 255)},${Math.round(
-    Math.random() * 255
+      Math.random() * 255
   )},${Math.round(Math.random() * 255)})`;
 };
 
@@ -21,20 +21,20 @@ function drawCode() {
   ctx.fillStyle = ramColor();
   ctx.font = "70px Helvetica";
   ctx.fillText(
-    code,
-    Math.round((Math.random() * can.height) / 2),
-    Math.round((Math.random() * can.width) / 2)
+      code,
+      Math.round((Math.random() * can.height) / 2),
+      Math.round((Math.random() * can.width) / 2)
   );
   for (let i = 0; i < Math.round(Math.random() * 4); i++) {
     ctx.beginPath();
     ctx.lineWidth = 10;
     ctx.moveTo(
-      Math.round(Math.random() * can.height),
-      Math.round(Math.random() * can.width)
+        Math.round(Math.random() * can.height),
+        Math.round(Math.random() * can.width)
     );
     ctx.lineTo(
-      Math.round(Math.random() * can.height),
-      Math.round(Math.random() * can.width)
+        Math.round(Math.random() * can.height),
+        Math.round(Math.random() * can.width)
     );
     ctx.strokeStyle = ramColor();
     ctx.stroke();
@@ -59,68 +59,68 @@ $(function () {
   var vCodeLabel = $("#vCodeLabel");
   var register = $("#register");
   passName
-    .on("focus", function () {
-      passNameLabel.text("请输入密码");
-      passNameLabel.css({ color: "#70A4CC" });
-    })
-    .on("blur", function () {
-      passNameLabel.text("密码");
-      passNameLabel.css({ color: "#70A4CC" });
-      if (curPassName != "" || curPassName != null) {
+      .on("focus", function () {
+        passNameLabel.text("请输入密码");
+        passNameLabel.css({color: "#70A4CC"});
+      })
+      .on("blur", function () {
+        passNameLabel.text("密码");
+        passNameLabel.css({color: "#70A4CC"});
+        if (curPassName != "" || curPassName != null) {
+          if (curPassName.val() === null || curPassName.val() === "") {
+            curPassNameLabel.text("再次输入密码");
+            curPassNameLabel.css({color: "#70A4CC"});
+          } else if (passName.val() === curPassName.val()) {
+            console.log(2);
+            curPassNameLabel.text("密码一致");
+            curPassNameLabel.css({color: "#0f0"});
+          } else {
+            console.log(3);
+            curPassNameLabel.text("两次密码输入不一致，请重新输入");
+            curPassNameLabel.css({color: "#f00"});
+          }
+        }
+      });
+  
+  curPassName
+      .on("focus", function () {
+        console.log(1);
+        curPassNameLabel.text("请输入与上面一致的密码");
+        curPassNameLabel.css({color: "#70A4CC"});
+      })
+      .on("blur", function () {
         if (curPassName.val() === null || curPassName.val() === "") {
           curPassNameLabel.text("再次输入密码");
-          curPassNameLabel.css({ color: "#70A4CC" });
+          curPassNameLabel.css({color: "#70A4CC"});
         } else if (passName.val() === curPassName.val()) {
           console.log(2);
           curPassNameLabel.text("密码一致");
-          curPassNameLabel.css({ color: "#0f0" });
+          curPassNameLabel.css({color: "#0f0"});
         } else {
           console.log(3);
           curPassNameLabel.text("两次密码输入不一致，请重新输入");
-          curPassNameLabel.css({ color: "#f00" });
+          curPassNameLabel.css({color: "#f00"});
         }
-      }
-    });
-
-  curPassName
-    .on("focus", function () {
-      console.log(1);
-      curPassNameLabel.text("请输入与上面一致的密码");
-      curPassNameLabel.css({ color: "#70A4CC" });
-    })
-    .on("blur", function () {
-      if (curPassName.val() === null || curPassName.val() === "") {
-        curPassNameLabel.text("再次输入密码");
-        curPassNameLabel.css({ color: "#70A4CC" });
-      } else if (passName.val() === curPassName.val()) {
-        console.log(2);
-        curPassNameLabel.text("密码一致");
-        curPassNameLabel.css({ color: "#0f0" });
-      } else {
-        console.log(3);
-        curPassNameLabel.text("两次密码输入不一致，请重新输入");
-        curPassNameLabel.css({ color: "#f00" });
-      }
-    });
-
+      });
+  
   vCode
-    .on("focus", function () {
-      vCodeLabel.text("请输入验证码");
-      vCodeLabel.css({ color: "#70A4CC" });
-    })
-    .on("blur", function () {
-      if (vCode.val() === null || vCode.val() === "") {
-        vCodeLabel.text("验证码");
-        vCodeLabel.css({ color: "#70A4CC" });
-      } else if (vCode.val() !== code) {
-        vCodeLabel.text("验证码错误");
-        vCodeLabel.css({ color: "#f00" });
-      } else if (vCode.val() === code) {
-        vCodeLabel.text("验证码正确");
-        vCodeLabel.css({ color: "#0f0" });
-      }
-    });
-
+      .on("focus", function () {
+        vCodeLabel.text("请输入验证码");
+        vCodeLabel.css({color: "#70A4CC"});
+      })
+      .on("blur", function () {
+        if (vCode.val() === null || vCode.val() === "") {
+          vCodeLabel.text("验证码");
+          vCodeLabel.css({color: "#70A4CC"});
+        } else if (vCode.val() !== code) {
+          vCodeLabel.text("验证码错误");
+          vCodeLabel.css({color: "#f00"});
+        } else if (vCode.val() === code) {
+          vCodeLabel.text("验证码正确");
+          vCodeLabel.css({color: "#0f0"});
+        }
+      });
+  
   register.on("click", function () {
     if (passName.val() === curPassName.val() && vCode.val() === code) {
       console.log("re1");
@@ -128,7 +128,7 @@ $(function () {
         type: "POST",
         url: "/template/user.do",
         data: {
-          method: "registe",
+          method: "register",
           userName: userName.val(),
           passName: passName.val(),
         },
@@ -136,7 +136,7 @@ $(function () {
         success: function (response) {
           if (response.result === "success") {
             alert("注册成功，按确定返回登录界面");
-            $(location).attr({ href: "login.html" });
+            $(location).attr({href: "login.html"});
           } else {
             alert("注册失败，请重新尝试");
           }
