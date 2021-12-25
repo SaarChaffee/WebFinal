51fe7129dbcae773f92f3a16f3bd44deecad357b
SaarChaffee <1240148332@qq.com>
2021-12-25 18:19:45 +0800

    finish login function

--- src/main/webapp/static/js/login.js
+++ src/main/webapp/static/js/login.js
@@ -11,32 +11,35 @@ $(function () {
   var passName = $("#passName");
   var passNameLabel = $("#passNameLabel");
   var login = $("#login");
-  
+
   userName
-      .on("focus", function () {
-        userNameLabel.text("请输入用户名");
-        vCodeLabel.css({color: "#70A4CC"});
-      })
-      .on("blur", function () {
-        userNameLabel.text("用户名");
-        vCodeLabel.css({color: "#70A4CC"});
-      });
+    .on("focus", function () {
+      userNameLabel.text("请输入用户名");
+      userNameLabel.css({ color: "#70A4CC" });
+    })
+    .on("blur", function () {
+      userNameLabel.text("用户名");
+      userNameLabel.css({ color: "#70A4CC" });
+    });
   passName
-      .on("focus", function () {
-        passNameLabel.text("请输入密码");
-        vCodeLabel.css({color: "#70A4CC"});
-      })
-      .on("blur", function () {
-        passNameLabel.text("密码");
-        vCodeLabel.css({color: "#70A4CC"});
-      });
-  
+    .on("focus", function () {
+      passNameLabel.text("请输入密码");
+      passNameLabel.css({ color: "#70A4CC" });
+    })
+    .on("blur", function () {
+      passNameLabel.text("密码");
+      passNameLabel.css({ color: "#70A4CC" });
+    });
+
   login.on("click", function () {
     if (userName.val() === null || userName.val() === "") {
+      console.log("lo1");
       userName.focus();
     } else if (passName.val() === null || passName.val() === "") {
+      console.log("lo2");
       passName.focus();
     } else {
+      console.log("lo3");
       $.ajax({
         type: "GET",
         url: "/user.do",
@@ -49,12 +52,12 @@ $(function () {
         success: function (response) {
           if (response.result === "success") {
             alert("登陆成功，按确定进入主页");
-            $(location).attr({href: "template/home.html"});
+            $(location).attr({ href: "template/home.html" });
           } else if (response.result === "wrong") {
             userNameLabel.text("用户名或密码错误");
-            userNameLabel.css({color: "#f00"});
+            userNameLabel.css({ color: "#f00" });
             passNameLabel.text("用户名或密码错误");
-            passNameLabel.css({color: "#f00"});
+            passNameLabel.css({ color: "#f00" });
           }
         },
       });
