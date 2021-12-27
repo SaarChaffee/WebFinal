6ad1884410c568ab7be72a1f76dc2e2f707cbe51
SaarChaffee <1240148332@qq.com>
2021-12-27 08:55:24 +0800

    judge user exist when login

--- src/main/webapp/static/js/login.js
+++ src/main/webapp/static/js/login.js
@@ -11,26 +11,47 @@ $(function () {
   var passName = $("#passName");
   var passNameLabel = $("#passNameLabel");
   var login = $("#login");
-
+  
   userName
-    .on("focus", function () {
-      userNameLabel.text("请输入用户名");
-      userNameLabel.css({ color: "#70A4CC" });
-    })
-    .on("blur", function () {
-      userNameLabel.text("用户名");
-      userNameLabel.css({ color: "#70A4CC" });
-    });
+      .on("focus", function () {
+        userNameLabel.text("请输入用户名");
+        userNameLabel.css({color: "#70A4CC"});
+      })
+      .on("blur", function () {
+        if (userName.val() === "" || userName.val() === null) {
+          userNameLabel.text("用户名");
+          userNameLabel.css({color: "#70A4CC"});
+        } else {
+          $.ajax({
+            type: "GET",
+            url: "/user.do",
+            data: {
+              method: "isUserExist",
+              username: userName.val(),
+            },
+            dataType: "json",
+            success: function (response) {
+              if (response.result === "used") {
+                userNameLabel.text("用户名");
+                userNameLabel.css({color: "#70A4CC"});
+              } else if (response.result === "free") {
+                userNameLabel.text("用户不存在");
+                userNameLabel.css({color: "#f00"});
+              }
+            },
+          });
+        }
+      });
   passName
-    .on("focus", function () {
-      passNameLabel.text("请输入密码");
-      passNameLabel.css({ color: "#70A4CC" });
-    })
-    .on("blur", function () {
-      passNameLabel.text("密码");
-      passNameLabel.css({ color: "#70A4CC" });
-    });
-
+      .on("focus", function () {
+        passNameLabel.text("请输入密码");
+        passNameLabel.css({color: "#70A4CC"});
+      })
+      .on("blur", function () {
+        passNameLabel.text("密码");
+        passNameLabel.css({color: "#70A4CC"});
+      });
+  
   login.on("click", function () {
     if (userName.val() === null || userName.val() === "") {
       console.log("lo1");
@@ -52,12 +73,12 @@ $(function () {
         success: function (response) {
           if (response.result === "success") {
             alert("登陆成功，按确定进入主页");
-            $(location).attr({ href: "template/home.html" });
+            $(location).attr({href: "template/home.html"});
           } else if (response.result === "wrong") {
             userNameLabel.text("用户名或密码错误");
-            userNameLabel.css({ color: "#f00" });
+            userNameLabel.css({color: "#f00"});
             passNameLabel.text("用户名或密码错误");
-            passNameLabel.css({ color: "#f00" });
+            passNameLabel.css({color: "#f00"});
           }
         },
       });
